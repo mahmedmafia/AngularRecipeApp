b34b8a352ee8928cbebfb6ff2da59b01eb1e0426
Your Name <faridmahmed3@gmail.com>
2020-02-04 00:29:14 +0200

    adding lazy loading app module
    adding lazy loading app Routing  module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
-
-
 const routes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
     { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
     { path: 'shoppinglist', loadChildren: './ShoppingList/shopping-list.module#ShoppingListModule' },
     { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
 
-    // { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+    imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
